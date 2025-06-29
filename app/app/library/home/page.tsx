4968388b05e4a791aaf5223210b9a4708642ee1e
Oscar May <67429870+OscarMayReal@users.noreply.github.com>
2025-06-29 11:51:02 +0100

    notebook page

--- app/app/library/home/page.tsx
+++ app/app/library/home/page.tsx
@@ -1,10 +1,35 @@
-import { authClient } from "@/lib/auth-client";
+"use client"
+import { Note } from "@/generated/prisma"
+import { useContext } from "react"
+import { notebookListContext } from "../layout"
+import { NotebookIcon } from "lucide-react"
 
 export default function Home() {
+    const notebooks = useContext(notebookListContext)
     return (
         <div>
-            <h1>Home</h1>
-            <button onClick={() => authClient.signOut()}>Sign out</button>
+            <NotebookList notebooks={notebooks} />
+        </div>
+    )
+}
+
+function NotebookList({ notebooks }: { notebooks: { data: Note[], loaded: boolean, reload: () => void } | null }) {
+    return (
+        <div className="notebook_list">
+            {notebooks?.loaded && notebooks?.data.map((notebook: Note) => (
+                <NotebookListItem key={notebook.id} notebook={notebook} />
+            ))}
+            {!notebooks?.loaded && <div>Loading...</div>}
+            {notebooks?.loaded && notebooks?.data.length === 0 && <div>No notebooks</div>}
+        </div>
+    )
+}
+
+function NotebookListItem({ notebook }: { notebook: Note }) {
+    return (
+        <div className="notebook_list_item">
+            <NotebookIcon size={20} />
+            <div>{notebook.title}</div>
         </div>
     )
 }
